c365bdbffa1a43878cc7ff2d8e0358d918220881
jcnpereira <casimiro@ipt.pt>
2025-03-31 22:05:04 +0100

    Cache

--- app/src/main/java/pt/ipt/dama/storage/MainActivity.kt
+++ app/src/main/java/pt/ipt/dama/storage/MainActivity.kt
@@ -1,10 +1,10 @@
 package pt.ipt.dama.storage
 
-import android.view.inputmethod.InputMethodManager
 import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -13,13 +13,25 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.snackbar.Snackbar
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileNotFoundException
+import java.io.FileOutputStream
+import java.io.PrintStream
+import java.util.Scanner
 
 class MainActivity : AppCompatActivity() {
 
     // Shared Preferences
     private lateinit var txt1: EditText
+
+    // Shared Preferences, múltiplos ficheiros
     private lateinit var txt2nome: EditText
     private lateinit var txt2idade: EditText
+
+    // Cache
+    private lateinit var txt3nome: EditText
+    private lateinit var txt3idade: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -53,6 +65,17 @@ class MainActivity : AppCompatActivity() {
             lerSharedPrefencesMulti()
         }
 
+        // Cache
+        // tarefa 3
+        txt3nome = findViewById(R.id.editText3a)
+        txt3idade = findViewById(R.id.editText3b)
+        findViewById<Button>(R.id.btWrite3).setOnClickListener {
+            escreveCache()
+            esconderTeclado(it)
+        }
+        findViewById<Button>(R.id.btRead3).setOnClickListener {
+            lerCache()
+        }
     }
 
 
@@ -111,7 +134,10 @@ class MainActivity : AppCompatActivity() {
         // Os dados são sempre escritos no formato: 'Variável'+'valor'
         // usa-se o método putXXXXX, onde XXXXX significa String, Int, Double, etc.
         editorNomes.putString("NOME", txt2nome.text.toString())
-        editorIdades.putInt("IDADE", txt2idade.text.toString().toIntOrNull() ?: 0) // se a Idade=NULL, coloca 0
+        editorIdades.putInt(
+            "IDADE",
+            txt2idade.text.toString().toIntOrNull() ?: 0
+        ) // se a Idade=NULL, coloca 0
         // guardar as alterações
         editorNomes.commit()  // eventualmente, <=>   editorNomes.apply()
         editorIdades.commit()
@@ -126,8 +152,8 @@ class MainActivity : AppCompatActivity() {
      * na Shared Preferences
      */
     private fun lerSharedPrefencesMulti() {
-        val sharedPreferencesNome = getSharedPreferences("nomes.dat",MODE_PRIVATE)
-        val sharedPreferencesIdade = getSharedPreferences("idades.dat",MODE_PRIVATE)
+        val sharedPreferencesNome = getSharedPreferences("nomes.dat", MODE_PRIVATE)
+        val sharedPreferencesIdade = getSharedPreferences("idades.dat", MODE_PRIVATE)
         // ler o conteúdo da Shared Preferences. Se não for possível a leitura, devolve um texto alternativo
         // estou a usar o método getString() porque guardei 'texto'. Outros tipos de dados,
         // implicam outros métodos
@@ -136,7 +162,64 @@ class MainActivity : AppCompatActivity() {
 
         // usar os dados lidos
         val contextView = findViewById<View>(R.id.main)
-        Snackbar.make(contextView, "Você chama-se ${nome.toString().uppercase()} e tem $idade anos!", Snackbar.LENGTH_SHORT).show()
+        Snackbar.make(
+            contextView,
+            "Você chama-se ${nome.toString().uppercase()} e tem $idade anos!",
+            Snackbar.LENGTH_SHORT
+        ).show()
+    }
+
+
+    // TAREFA 3
+    /**
+     * Escrever os valores introduzidos pelo utilizador na Cache
+     */
+    private fun escreveCache() {
+        // endereço da pasta onde será gerado o ficheiro
+        // associado à área de atuação da aplicação
+        val directory: File = cacheDir
+        // nome do ficheiro na Cache
+        val file: File = File(directory, "dadosCache.txt")
+        try {
+            val fo: FileOutputStream = FileOutputStream(file)
+            val ps: PrintStream = PrintStream(fo)
+            ps.println(txt3nome.text)
+            ps.println(txt3idade.text)
+            ps.close()
+            fo.close()
+
+            // neste caso, vamos mostrar uma mensagem de 'conforto' ao utilizador
+            Toast.makeText(this, getString(R.string.dados_guardados), Toast.LENGTH_SHORT).show()
+        } catch (e: FileNotFoundException) {
+            Toast.makeText(this, getString(R.string.escritaErradaCache), Toast.LENGTH_LONG).show()
+        }
+    }
+
+    /**
+     * Ler os valores introduzidos pelo utilizador da Cache
+     */
+    private fun lerCache() {
+        val directory: File = cacheDir // <=> getCacheDir()
+        val file = File(directory, "dadosCache.txt")
+        try {
+            val fi = FileInputStream(file)
+            val sc = Scanner(fi)
+            val nome = sc.nextLine()
+            val idade = sc.nextLine()
+            sc.close()
+            fi.close()
+
+            // mostrar os dados
+            val contextView = findViewById<View>(R.id.main)
+            Snackbar.make(
+                contextView,
+                "Você chama-se ${nome.toString().uppercase()} e tem $idade anos!",
+                Snackbar.LENGTH_LONG
+            ).show()
+        } catch (e: FileNotFoundException) {
+            Toast.makeText(this, getString(R.string.cache_vazia), Toast.LENGTH_LONG).show()
+        }
+
     }
 
 
